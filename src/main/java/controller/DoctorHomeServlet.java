ee6fbf586c1affc8c79caece1604975dc2ef95a8
Pluu120104 <phongluu120104@gmail.com>
2025-05-27 02:00:19 +0700

    Update DoctorHomeServlet

--- src/main/java/controller/DoctorHomeServlet.java
+++ src/main/java/controller/DoctorHomeServlet.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Appointment;
 import view.AppointmentDAO;
 import model.Employee;
 import view.SimpleDBContext;
@@ -9,24 +10,35 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.List;
 
 @WebServlet("/doctor-home")
 public class DoctorHomeServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         HttpSession session = request.getSession();
-        Employee doctor = (Employee) session.getAttribute("acount");
+        Object acc = session.getAttribute("account");
+        String role = (String) session.getAttribute("login-as");
+
+        if (acc == null || !(acc instanceof Employee) || ((Employee) acc).getRoleId() != 1) {
+            response.sendRedirect("Login.jsp");
+            return;
+        }
+
+        Employee doctor = (Employee) acc;
 
         try(Connection conn = new SimpleDBContext() {}.getConn()){
             AppointmentDAO dao = new AppointmentDAO(conn);
-            int[] stats = dao.getTodayStatsByDoctorId(doctor.getEmployeeId());
-
-            request.setAttribute("stats", stats);
+            List<Appointment> list = dao.getAppointmentsByDoctorId(doctor.getEmployeeId());
             request.setAttribute("doctor", doctor);
-            request.getRequestDispatcher("WEB-INF/DoctorHome.jsp").forward(request, response);
+            request.setAttribute("appointments", list);
+            request.getRequestDispatcher("Doctor_View/DoctorHome.jsp").forward(request, response);
         }catch (Exception e){
             e.printStackTrace();
             response.sendRedirect("error.jsp");
         }
+
     }
 }
